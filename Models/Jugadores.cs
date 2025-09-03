be55c6a7ca379e103ca152b8db274bf0a7912b54
JP-Guillen <juan_guillen@ucne.edu.do>
2025-09-02 21:24:24 -0400

    Creando el modelo

---
+++ Models/Jugadores.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroJugadores.Models
+{
+    public class Jugadores
+    {
+        [Key]
+        public int Idjugador { get; set; }
+
+        [Required(ErrorMessage = "El campo 'Nombres' es obligatorio.")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage= "EL Campo 'Partidas' es obigatorio.")]
+        [Range(0, int.MaxValue, ErrorMessage = "El numero de partidas debe ser un valor valido. ")]
+        public int Partidas { get; set; }
+
+    }
+}
