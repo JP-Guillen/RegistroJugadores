f4dcb9465227026d02c1bf79d92e0ed8fcccf665
JP-Guillen <juan_guillen@ucne.edu.do>
2025-09-15 20:32:32 -0400

    Modificando la variable Partidas por Victorias, agregando victoria, empates y derrotas

--- Models/Jugadores.cs
+++ Models/Jugadores.cs
@@ -10,9 +10,11 @@ namespace RegistroJugadores.Models
         [Required(ErrorMessage = "El campo 'Nombres' es obligatorio.")]
         public string Nombre { get; set; }
 
-        [Required(ErrorMessage= "EL Campo 'Partidas' es obigatorio.")]
-        [Range(0, int.MaxValue, ErrorMessage = "El numero de partidas debe ser un valor valido. ")]
-        public int Partidas { get; set; }
+        public int Victorias { get; set; } = 0;
+
+        public int Empates { get; set; } = 0;
+
+        public int Derrotas { get; set; } = 0;
 
     }
 }
